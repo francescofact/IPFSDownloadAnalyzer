c05a0482c6835b22185578fb2f89d80e040890cd
Francesco <ffattori@me.com>
2021-04-08 12:55:12 +0200

    catch 500 error during peer request

--- static/app.js
+++ static/app.js
@@ -2,6 +2,7 @@
 let download_size = false;
 let daemonInterval = null;
 let cache = {};
+let loop = true;
 
 //initialize stuff
 //window.setInterval(daemonStatus(), 5000);
@@ -27,28 +28,29 @@ function daemonStatus(){
 //function to update download status and cache peer help
 function watchdog(){
     //get peers
-    let loop = true;
-    $.get("/api/peers", function(data, status){
-        if (status === "success"){
-            if (data["status"] === "idle"){
+    loop = true;
+    $.ajax({
+        url: "/api/peers",
+        success: function(data) {
+            if (data["status"] === "idle") {
                 alert("Download Completed!");
                 loop = false;
                 $('#progressbar').css('width', '100%').attr('aria-valuenow', 100);
             } else {
                 let peers = data["peers"];
                 //update cache and datatable
-                Object.keys(peers).forEach(function(key) {
+                Object.keys(peers).forEach(function (key) {
                     let peer = peers[key]
                     //datatables update
-                    if (key in cache){
+                    if (key in cache) {
                         let skip = false;
-                        table.rows().every( function (index) {
-                            if (!skip){
-                               let row = this.data();
-                                if (row[1] === key){
-                                   row[3] = bytesToSize(peer[2][1]);
-                                   table.row(index).data(row).draw(false);
-                                   skip = true;
+                        table.rows().every(function (index) {
+                            if (!skip) {
+                                let row = this.data();
+                                if (row[1] === key) {
+                                    row[3] = bytesToSize(peer[2][1]);
+                                    table.row(index).data(row).draw(false);
+                                    skip = true;
                                 }
                             }
 
@@ -68,16 +70,17 @@ function watchdog(){
                 //calc downloaded and update UI
                 let downloaded = calc_downloaded();
                 $("#downloaded_label").html(bytesToSize(downloaded));
-                let percent = ( 100 * downloaded) / download_size;
+                let percent = (100 * downloaded) / download_size;
                 $("#progress_label").html(percent.toFixed(2));
-                $('#progressbar').css('width', percent+'%').attr('aria-valuenow', percent);
+                $('#progressbar').css('width', percent + '%').attr('aria-valuenow', percent);
 
             }
+        },
+        complete: function() {
+            if (loop)
+                setTimeout(watchdog, 5000);
         }
-        if (loop)
-            setTimeout(watchdog, 5000);
     });
-
 }
 
 //get downloaded part summing peers in cache
