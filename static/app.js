7589e52fdcb50a07f35ed8d60f01005ff67afecb
Francesco <ffattori@me.com>
2021-04-08 13:50:17 +0200

    added decimals to file sizes in the frontend

--- static/app.js
+++ static/app.js
@@ -97,7 +97,7 @@ function bytesToSize(bytes) {
    var sizes = ['bytes', 'kb', 'mb', 'gb', 'tb'];
    if (bytes == 0) return '0 Byte';
    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+   return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
 }
 
 //start download
